4803df52316017aa3950158ca49c82a9abc17f6c
Abiam <abiamrg.14@gmail.com>
2022-03-03 16:34:15 +0000

    Commit inicial

---
+++ src/main/java/com/example/red_social_medicos/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.red_social_medicos;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
